871a6fcc70cfc6b3241781833861544bd4b2c081
Ajakaye <mailpaulade@gmail.com>
2021-02-13 19:11:29 +0100

    fixed settings

--- djangopwa/settings.py
+++ djangopwa/settings.py
@@ -93,13 +93,27 @@ WSGI_APPLICATION = 'djangopwa.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+APP_ENVIRONMENT = os.getenv("APP_ENVIRONMENT","local")
 
+if APP_ENVIRONMENT == "local":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+    
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('USER'),
+            'PASSWORD': os.getenv('PASSWORD'),
+            'HOST': os.getenv('HOST'),
+            'PORT': os.getenv('DB_PORT')
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
