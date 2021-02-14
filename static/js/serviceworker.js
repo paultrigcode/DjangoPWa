489c5c2fc02c11b624cb7674c5c02c8225f80c8f
Ajakaye <mailpaulade@gmail.com>
2021-02-14 12:55:32 +0100

    seems like ive completed splash

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -16,7 +16,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin) {
-      if ((requestUrl.pathname === '/')) {
+      if ((requestUrl.pathname === './')) {
         event.respondWith(caches.match('/base_layout'));
         return;
       }
